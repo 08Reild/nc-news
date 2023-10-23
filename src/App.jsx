05540b875adfbd3f648baf73493a6d6961e9acfd
me <danielwong@Daniels-MacBook-Air-2.local>
2023-10-23 14:45:10 +0100

    basic structure of homepage complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from './components/navbar'
+import SearchBar from './components/searchbar'
+import Header from './components/header'
+import About from './components/about'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <NavBar></NavBar>
+    <SearchBar></SearchBar>
+    <Header></Header>
+    <About></About>
     </>
   )
 }
---
+++ src/components/about.jsx
@@ -0,0 +1,9 @@
+function About () {
+    return (
+        <>
+        <h2>Welcome to NC news, the home of user created articles. Create, share and interact with articles today!</h2>
+        </>
+    )
+}
+
+export default About
